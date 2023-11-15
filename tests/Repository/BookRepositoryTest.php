54dbeb9fd531bdb95b0e5abdef497279ffa76042
Dmitry <leik.83@mail.ru>
2023-11-15 10:12:20 +0300

    Update all tests

---
+++ tests/Repository/BookRepositoryTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Entity\Book;
+use App\Entity\BookCategory;
+use App\Repository\BookRepository;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class BookRepositoryTest extends KernelTestCase
+{
+    private BookRepository $bookRepository;
+    protected ?EntityManagerInterface $entityManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
+        $this->bookRepository = $this->getRepositoryForEntity(Book::class);
+    }
+
+    public function testFindByCategory(): void
+    {
+        $deviceCategory = (new BookCategory())->setTitle('Devices')->setSlug('devices');
+        $this->entityManager->persist($deviceCategory);
+
+        for ($i = 0; $i < 5; ++$i) {
+            $book = $this->createBook('device'.$i, $deviceCategory);
+            $this->entityManager->persist($book);
+        }
+
+        $this->entityManager->flush();
+
+        $this->assertCount(5, $this->bookRepository->findByCategory($deviceCategory->getId()));
+    }
+
+    private function createBook(string $name, BookCategory $bookCategory): Book
+    {
+        return (new Book())
+            ->setPublicationDate(new DateTime())
+            ->setAuthors(['author'])
+            ->setMeap(false)
+            ->setSlug($name)
+            ->setImage('test.png')
+            ->setTitle('test')
+            ->setCategories(new ArrayCollection([$bookCategory]));
+    }
+
+    protected function getRepositoryForEntity(string $entityClass): mixed
+    {
+        return $this->entityManager->getRepository($entityClass);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+}
