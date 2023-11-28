6fe5e660069b6479d2d73d183a979af3f0238de1
Dmitry <leik.83@mail.ru>
2023-11-28 20:07:24 +0300

    [#4] Fixed all tests

--- migrations/Version20231128170625.php
+++ migrations/Version20231128170625.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231128152348 extends AbstractMigration
+final class Version20231128170625 extends AbstractMigration
 {
     public function getDescription(): string
     {
